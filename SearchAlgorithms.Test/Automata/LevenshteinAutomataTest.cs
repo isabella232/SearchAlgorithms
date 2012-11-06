1db21711aa21370ab6de834afa346455fc09737a
Eric LaForce <eric@skookum.com>
2012-11-06 07:03:14 -0500

    Added a couple unit tests

--- SearchAlgorithms.Test/Automata/LevenshteinAutomataTest.cs
+++ SearchAlgorithms.Test/Automata/LevenshteinAutomataTest.cs
@@ -17,18 +17,18 @@ namespace SearchAlgorithms.Test.Automata
         public void NfaIsCreatedWithNoError()
         {
             Nfa nfa = _automata.Construct();
-            string dotfile = nfa.WriteGraph();
             Assert.IsNotNull(nfa);
-            Assert.IsNotNull(dotfile);
+            string dotfile = nfa.WriteGraph();
+            Assert.AreEqual(_nfaDotFile, dotfile);
         }
 
         [Test]
         public void DfaIsCreatedWithNoError()
         {
             Dfa dfa = _automata.Construct().ConstructDfaUsingPowerSet();
-            string dotfile = dfa.WriteGraph();
             Assert.IsNotNull(dfa);
-            Assert.IsNotNull(dotfile);
+            string dotfile = dfa.WriteGraph();
+            Assert.AreEqual(_dfaDotFile, dotfile);
         }
 
         [Test]
@@ -40,5 +40,65 @@ namespace SearchAlgorithms.Test.Automata
             string next = dfa.FindNextValidString("gice");
             Assert.AreEqual(next, "gice");
         }
+
+        private string _nfaDotFile = "digraph nfa {\n" +
+            "rankdir=LR;\n" +
+            "node [shape = circle];\n" +
+            "N0_0 -> N1_0 [ label = \"n\" ];\n" +
+            "N0_0 -> N0_1 [ label = \"Ä\" ];\n" +
+            "N0_0 -> N1_1 [ label = \"Ä\" ];\n" +
+            "N0_0 -> N1_1 [ label = \"Ë\" ];\n" +
+            "N0_1 -> N1_1 [ label = \"n\" ];\n" +
+            "N1_0 -> N2_0 [ label = \"i\" ];\n" +
+            "N1_0 -> N1_1 [ label = \"Ä\" ];\n" +
+            "N1_0 -> N2_1 [ label = \"Ä\" ];\n" +
+            "N1_0 -> N2_1 [ label = \"Ë\" ];\n" +
+            "N1_1 -> N2_1 [ label = \"i\" ];\n" +
+            "N2_0 -> N3_0 [ label = \"c\" ];\n" +
+            "N2_0 -> N2_1 [ label = \"Ä\" ];\n" +
+            "N2_0 -> N3_1 [ label = \"Ä\" ];\n" +
+            "N2_0 -> N3_1 [ label = \"Ë\" ];\n" +
+            "N2_1 -> N3_1 [ label = \"c\" ];\n" +
+            "N3_0 -> N4_0 [ label = \"e\" ];\n" +
+            "N3_0 -> N3_1 [ label = \"Ä\" ];\n" +
+            "N3_0 -> N4_1 [ label = \"Ä\" ];\n" +
+            "N3_0 -> N4_1 [ label = \"Ë\" ];\n" +
+            "N3_1 -> N4_1 [ label = \"e\" ];\n" +
+            "N4_0 -> N4_1 [ label = \"Ä\" ];\n" +
+            "}";
+
+            private string _dfaDotFile = "digraph dfa {\n" +
+            "node [shape = circle];\n" +
+            "N0_0_1_1 -> N0_1_1_1;\n" +
+            "N1_0_0_1_1_1_2_1 -> N1_1_2_1;\n" +
+            "N2_0_1_1_2_1_3_1 -> N2_1_3_1;\n" +
+            "N3_0_2_1_3_1_4_1 -> N3_1_4_1;\n" +
+            "N4_0_3_1_4_1 -> N4_1;\n" +
+            "N0_0_1_1 -> N1_0_0_1_1_1_2_1 [ label = \"n\" ];\n" +
+            "N0_0_1_1 -> N0_1_1_1_2_1 [ label = \"i\" ];\n" +
+            "N0_1_1_1_2_1 -> N1_1 [ label = \"n\" ];\n" +
+            "N0_1_1_1_2_1 -> N2_1 [ label = \"i\" ];\n" +
+            "N0_1_1_1_2_1 -> N3_1 [ label = \"c\" ];\n" +
+            "N3_1 -> N4_1 [ label = \"e\" ];\n" +
+            "N2_1 -> N3_1 [ label = \"c\" ];\n" +
+            "N1_1 -> N2_1 [ label = \"i\" ];\n" +
+            "N0_1_1_1 -> N1_1 [ label = \"n\" ];\n" +
+            "N0_1_1_1 -> N2_1 [ label = \"i\" ];\n" +
+            "N1_0_0_1_1_1_2_1 -> N2_0_1_1_2_1_3_1 [ label = \"i\" ];\n" +
+            "N1_0_0_1_1_1_2_1 -> N1_1_2_1_3_1 [ label = \"c\" ];\n" +
+            "N1_1_2_1_3_1 -> N2_1 [ label = \"i\" ];\n" +
+            "N1_1_2_1_3_1 -> N3_1 [ label = \"c\" ];\n" +
+            "N1_1_2_1_3_1 -> N4_1 [ label = \"e\" ];\n" +
+            "N1_1_2_1 -> N2_1 [ label = \"i\" ];\n" +
+            "N1_1_2_1 -> N3_1 [ label = \"c\" ];\n" +
+            "N2_0_1_1_2_1_3_1 -> N3_0_2_1_3_1_4_1 [ label = \"c\" ];\n" +
+            "N2_0_1_1_2_1_3_1 -> N2_1_3_1_4_1 [ label = \"e\" ];\n" +
+            "N2_1_3_1_4_1 -> N3_1 [ label = \"c\" ];\n" +
+            "N2_1_3_1_4_1 -> N4_1 [ label = \"e\" ];\n" +
+            "N2_1_3_1 -> N3_1 [ label = \"c\" ];\n" +
+            "N2_1_3_1 -> N4_1 [ label = \"e\" ];\n" +
+            "N3_0_2_1_3_1_4_1 -> N4_0_3_1_4_1 [ label = \"e\" ];\n" +
+            "N3_1_4_1 -> N4_1 [ label = \"e\" ];\n" +
+            "}";
     }
 }
